4b24712ccc2235c15952e08f5b2c8ed9dec1be81
vishalisharani <vishal@appsphere.in>
2020-03-11 11:46:22 +0530

    initial setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService, Message } from '../services/data.service';
+import * as AndroidPermissions from '@ionic-native/android-permissions';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +18,25 @@ export class HomePage {
 
   getMessages(): Message[] {
     return this.data.getMessages();
+  }
+
+  ionViewDidEnter() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = './assets/service.js';
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    AndroidPermissions.AndroidPermissions.checkPermission(AndroidPermissions.AndroidPermissions.PERMISSION.CAMERA).then(
+        result => console.log('Has permission?', result.hasPermission),
+        err => AndroidPermissions.AndroidPermissions.requestPermission(AndroidPermissions.AndroidPermissions.PERMISSION.CAMERA)
+    );
+
+    AndroidPermissions.AndroidPermissions.requestPermissions([
+        AndroidPermissions.AndroidPermissions.PERMISSION.CAMERA,
+        AndroidPermissions.AndroidPermissions.PERMISSION.MODIFY_AUDIO_SETTINGS
+    ]);
   }
 
 }
